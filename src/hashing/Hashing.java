ef0f519fe808d42ed0f2ef8aaab3daed27b370e9
fitriratna <fitriratna96@gmail.com>
2016-10-02 19:59:39 +0700

    Update Hashing.java

--- src/hashing/Hashing.java
+++ src/hashing/Hashing.java
@@ -30,66 +30,69 @@ public class Hashing {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
+        input();
+    }
+
+    private static void input() throws IOException {
         BufferedReader x = new BufferedReader(new InputStreamReader(System.in));
-        System.err.print("Masukan website : ");
-        String website = "";
-        website = x.readLine();
-        
-        String url_baru = "http://" + website;
-        Document doc = Jsoup.connect(url_baru).get();
-        //System.out.println(url_baru);
-        Elements teks = doc.select("body");
-        
-        String a = teks.text();
-        //System.out.println(a);
-        String tes = a.replaceAll("dan", "").replaceAll("tapi", "").replaceAll("[&,|]", "").replaceAll("dengan", "").replaceAll("yang", "");
-        //System.out.println(tes);
-        StringTokenizer b = new StringTokenizer(tes, " ");
-//        int c = 0;
-        
-        Vector<String> hasil = new Vector();
-        Vector<String> hasil2 = new Vector();
-        Vector hitung = new Vector();   
-        
-        while(b.hasMoreTokens()){
-            hasil.add(b.nextToken());
-            //System.out.println(b.nextToken());
+        System.out.print("Masukan website : ");
+        String website = x.readLine();
+        parsing(website);
+    }
+
+    private static void parsing(String url) throws IOException {
+        url = "http://" + url;
+        Document doc = Jsoup.connect(url).timeout(0).get();
+        Elements text = doc.select("body");
+        String teks = text.text();
+        teks = teks.replaceAll("[(-+.^:,'|&?!)]", "").replaceAll("yang", "").replaceAll("dengan", "").replaceAll("dan", "").replaceAll("dari", "");
+        String[] kata = teks.split(" ");
+
+        //-----------------------------------
+        String[] kata1 = new String[kata.length];
+        for (int i = 0; i < kata.length; i++) {
+            kata1[i] = kata[i];
         }
-//        
-        for (int i = 0; i < hasil.size(); i++) {
-            //System.err.println(hasil.get(i));
-            hasil2.add(hasil.get(i));
-            //System.err.println(hasil2.get(i));
-        }
-               //System.out.println(c);
-     
-        Hashtable data = new Hashtable();
-        
-        
-        for(Object key : hasil){
-            int angka = 0;
-            for(String key2 : hasil2){
-                if(key.equals(key2)){
-                    angka++;
+
+        Vector<String> index_kata = new <String>Vector();
+        Vector<Integer> hitung_kata = new <Integer>Vector();
+        for (int i = 0; i < kata1.length; i++) {
+            int hitung = 0;
+            for (int j = 1; j < kata.length; j++) {
+                if (kata1[i].equals(kata[j])) {
+                    hitung++;
                 }
             }
-            hitung.add(angka);
-        }
-        for (int i = 0; i < hasil.size(); i++) {
-            data.put(hasil.get(i), hitung.get(i));
-            //        System.out.println(data);
-        }
-        Enumeration k;
-        k = data.keys();
-        String akhir;
-        while (k.hasMoreElements()) {
-            akhir = (String) k.nextElement();
-            System.out.println(akhir + ": " + data.get(akhir));
+            if (index_kata.size() == 0) {
+                index_kata.add(kata1[i]);
+                hitung_kata.add(hitung);
+            } else {
+                int ada = 0;
+                for (int j = 0; j < index_kata.size(); j++) {
+                    if (kata1[i].equals(index_kata.get(j))) {
+                        ada++;
+                    }
+                }
+
+                if (ada == 0) {
+                    index_kata.add(kata1[i]);
+                    hitung_kata.add(hitung);
+                }
+            }
         }
 
+//        for (int i = 0; i < kata1.length; i++) {
+//            System.out.println(kata1[i]);
+//        }
 
-     //=====================================================================
+        System.out.println(kata.length + "<>" + index_kata.size());
+        Hashtable tabel_kata = new Hashtable();
+        for (int i = 0; i < index_kata.size(); i++) {
+            System.out.println(index_kata.get(i) + "=" + hitung_kata.get(i));
+            tabel_kata.put(index_kata.get(i),hitung_kata.get(i));
+        }
+        
         
     }
-    
+
 }
